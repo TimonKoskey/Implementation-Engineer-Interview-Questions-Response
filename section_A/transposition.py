47c50a7d52e2583522811f3d3cf7747d26b67b28
TimonKoskey <timonkosy92@gmail.com>
2021-06-21 13:36:18 +0300

    docker-compose errors

--- section_A/transposition.py
+++ section_A/transposition.py
@@ -1,14 +1,20 @@
 def split(seq, length):
+    # for i in range(0, len(seq), length):
+    #     print(i)
+    #     print(seq[i:i + length])
     return [seq[i:i + length] for i in range(0, len(seq), length)]
 
 def encode(key, text):
     order = {
       int(val): num for num, val in enumerate(key)
     }
+    # print(order)
     ciphertext = ''
 
     for index in sorted(order.keys()):
+        # print(index)
         for part in split(text, len(key)):
+            print(part)
             try:
                 ciphertext += part[order[index]]
             except IndexError:
